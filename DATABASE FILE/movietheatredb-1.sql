43de42260d680eafe5184cf49b7e5adad4819856
Mahesh lamsal <84889576+Romanmahesh@users.noreply.github.com>
2023-07-06 14:56:40 +0545

    Add files via upload

---
+++ DATABASE FILE/movietheatredb-1.sql
@@ -0,0 +1,401 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 07, 2022 at 11:15 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `movietheatredb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_bookings`
+--
+
+CREATE TABLE `tbl_bookings` (
+  `book_id` int(11) NOT NULL,
+  `ticket_id` varchar(30) NOT NULL,
+  `t_id` int(11) NOT NULL COMMENT 'theater id',
+  `user_id` int(11) NOT NULL,
+  `show_id` int(11) NOT NULL,
+  `screen_id` int(11) NOT NULL,
+  `no_seats` int(3) NOT NULL COMMENT 'number of seats',
+  `amount` int(5) NOT NULL,
+  `ticket_date` date NOT NULL,
+  `date` date NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_bookings`
+--
+
+INSERT INTO `tbl_bookings` (`book_id`, `ticket_id`, `t_id`, `user_id`, `show_id`, `screen_id`, `no_seats`, `amount`, `ticket_date`, `date`, `status`) VALUES
+(13, 'BKID2313964', 6, 5, 21, 6, 4, 2400, '2021-04-16', '2021-04-15', 1),
+(14, 'BKID1766021', 6, 5, 22, 6, 2, 1200, '2021-04-16', '2021-04-16', 1),
+(15, 'BKID3003797', 6, 6, 19, 5, 1, 300, '2022-06-28', '2022-06-28', 1),
+(16, 'BKID6041249', 6, 7, 21, 6, 1, 600, '2022-08-04', '2022-08-04', 1),
+(17, 'BKID8573267', 6, 7, 19, 5, 1, 300, '2022-08-04', '2022-08-04', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_login`
+--
+
+CREATE TABLE `tbl_login` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL COMMENT 'email',
+  `password` varchar(50) NOT NULL,
+  `user_type` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_login`
+--
+
+INSERT INTO `tbl_login` (`id`, `user_id`, `username`, `password`, `user_type`) VALUES
+(1, 0, 'admin', 'password', 0),
+(2, 3, 'theatre', 'password', 1),
+(3, 4, 'theatre2', 'password', 1),
+(12, 2, 'harryden@gmail.com', 'password', 2),
+(15, 14, 'USR295127', 'PWD195747', 1),
+(17, 4, 'bruno@gmail.com', 'password', 2),
+(18, 6, 'THR760801', 'PWD649976', 1),
+(19, 5, 'james@gmail.com', 'password', 2),
+(20, 6, 'mahesh@gmail.com', 'mahesh123', 2),
+(21, 7, 'THR818334', 'PWD877287', 1),
+(22, 8, 'THR500423', 'PWD223410', 1),
+(23, 7, 'bijay@gmail.com', 'BIJAY123', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_movie`
+--
+
+CREATE TABLE `tbl_movie` (
+  `movie_id` int(11) NOT NULL,
+  `t_id` int(11) NOT NULL COMMENT 'theatre id',
+  `movie_name` varchar(255) NOT NULL,
+  `cast` varchar(500) NOT NULL,
+  `desc` varchar(1000) NOT NULL,
+  `release_date` date NOT NULL,
+  `image` varchar(200) NOT NULL,
+  `video_url` varchar(200) NOT NULL,
+  `status` int(1) NOT NULL COMMENT '0 means active '
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_movie`
+--
+
+INSERT INTO `tbl_movie` (`movie_id`, `t_id`, `movie_name`, `cast`, `desc`, `release_date`, `image`, `video_url`, `status`) VALUES
+(1, 3, 'The Invisible Man', 'Elisabeth Moss, Oliver Jackson-Cohen, Aldis Hodge, Storm Reid', 'Cecilia\'s abusive ex-boyfriend fakes his death and becomes invisible to stalk and torment her. She begins experiencing strange events and decides to hunt down the truth on her own.', '2020-03-04', 'images/tim.jpg', 'https://www.youtube.com/watch?v=WO_FJdiY9dA', 0),
+(11, 6, 'Cherry', 'Tom Holland, Ciara Bravo, Harry Holland, Kelli Berglund', 'Cherry (Tom Holland) drifts from college dropout to army medic in Iraq-anchored only by his one true love, Emily (Ciara Bravo). But after returning from the war with PTSD, his life spirals into drugs.', '2021-03-01', 'images/cherry.jpg', 'https://www.youtube.com/watch?v=H5bH6O0bErk', 0),
+(12, 6, 'Godzilla vs. Kong', 'Millie Bobby Brown, Alexander Skarsgard, Rebecca Hall', 'The initial confrontation between the two titans -- instigated by unseen forces -- is only the beginning of the mystery that lies deep within the core of the planet.', '2021-03-31', 'images/gvkpster.jpg', 'https://www.youtube.com/watch?v=odM92ap8_c0', 0),
+(13, 6, 'Outside the Wire', 'Anthony Mackie, Damson Idris, Emily Beecham', 'In the near future, a drone pilot sent into a war zone finds himself paired up with a top-secret android officer on a mission to stop a nuclear attack.', '2021-01-28', 'images/otw.jpg', 'https://www.youtube.com/watch?v=u8ZsUivELbs', 0),
+(18, 3, 'Chakka panja', 'DIPAK Raj giri', 'Comdey movie', '2022-04-27', 'images/chakka panja.jpg', '', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_news`
+--
+
+CREATE TABLE `tbl_news` (
+  `news_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `cast` varchar(100) NOT NULL,
+  `news_date` date NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `attachment` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_news`
+--
+
+INSERT INTO `tbl_news` (`news_id`, `name`, `cast`, `news_date`, `description`, `attachment`) VALUES
+(3, 'Black Widow', 'Scarlett Johansson, Florence Pugh, David Harbour, Rachel Weisz', '2021-07-09', 'At birth the Black Widow (aka Natasha Romanova) is given to the KGB, which grooms her to become its ultimate operative.', 'news_images/blackwidow.jpg'),
+(9, 'Shang-Chi and the Legend of the Ten Rings', 'Simu Liu, Awkwafina, Tony Leung, Fala Chen, Micheele Yeoh', '2021-09-14', 'Shang-Chi is a master of numerous unarmed and weaponry-based wushu styles, including the use of the gun, nunchaku, and jian.', 'news_images/shangchi.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_registration`
+--
+
+CREATE TABLE `tbl_registration` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` varchar(12) NOT NULL,
+  `age` int(2) NOT NULL,
+  `gender` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_registration`
+--
+
+INSERT INTO `tbl_registration` (`user_id`, `name`, `email`, `phone`, `age`, `gender`) VALUES
+(2, 'Harry Den', 'harryden@gmail.com', '1247778540', 22, 'gender'),
+(4, 'Bruno', 'bruno@gmail.com', '7451112450', 30, 'gender'),
+(5, 'James', 'james@gmail.com', '7124445696', 25, 'gender'),
+(6, 'mahesh lamsal', 'mahesh@gmail.com', '9812923595', 19, 'gender'),
+(7, 'bijay', 'bijay@gmail.com', '9812923454', 21, 'gender');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_screens`
+--
+
+CREATE TABLE `tbl_screens` (
+  `screen_id` int(11) NOT NULL,
+  `t_id` int(11) NOT NULL COMMENT 'theatre id',
+  `screen_name` varchar(110) NOT NULL,
+  `seats` int(11) NOT NULL COMMENT 'number of seats',
+  `charge` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_screens`
+--
+
+INSERT INTO `tbl_screens` (`screen_id`, `t_id`, `screen_name`, `seats`, `charge`) VALUES
+(1, 3, 'Screen 1', 100, 350),
+(2, 3, 'Screen 2', 150, 310),
+(3, 4, 'Screen 1', 200, 380),
+(4, 14, 'Screen1', 34, 250),
+(5, 6, 'Demo Screen', 150, 300),
+(6, 6, 'IMX Screen', 200, 600),
+(7, 3, '3d', 50, 50);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_shows`
+--
+
+CREATE TABLE `tbl_shows` (
+  `s_id` int(11) NOT NULL,
+  `st_id` int(11) NOT NULL COMMENT 'show time id',
+  `theatre_id` int(11) NOT NULL,
+  `movie_id` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `status` int(11) NOT NULL COMMENT '1 means show available',
+  `r_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_shows`
+--
+
+INSERT INTO `tbl_shows` (`s_id`, `st_id`, `theatre_id`, `movie_id`, `start_date`, `status`, `r_status`) VALUES
+(19, 15, 6, 11, '2021-04-15', 0, 1),
+(20, 20, 6, 13, '2021-04-15', 0, 1),
+(21, 19, 6, 12, '2021-03-31', 1, 1),
+(22, 18, 6, 17, '2021-04-16', 1, 1),
+(23, 6, 3, 18, '2022-02-27', 1, 1),
+(24, 7, 3, 1, '2022-06-29', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_show_time`
+--
+
+CREATE TABLE `tbl_show_time` (
+  `st_id` int(11) NOT NULL,
+  `screen_id` int(11) NOT NULL,
+  `name` varchar(40) NOT NULL COMMENT 'noon,second,etc',
+  `start_time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_show_time`
+--
+
+INSERT INTO `tbl_show_time` (`st_id`, `screen_id`, `name`, `start_time`) VALUES
+(1, 1, 'Noon', '10:00:00'),
+(2, 1, 'Matinee', '14:00:00'),
+(3, 1, 'First', '18:00:00'),
+(4, 1, 'Second', '21:00:00'),
+(5, 2, 'Noon', '10:00:00'),
+(6, 2, 'Matinee', '14:00:00'),
+(7, 2, 'First', '18:00:00'),
+(8, 2, 'Second', '21:00:00'),
+(9, 3, 'Noon', '10:00:00'),
+(10, 3, 'Matinee', '14:00:00'),
+(11, 3, 'First', '18:00:00'),
+(12, 3, 'Second', '21:00:00'),
+(14, 4, 'Noon', '12:03:00'),
+(15, 5, 'First', '00:08:00'),
+(16, 5, 'Second', '15:10:00'),
+(17, 5, 'Others', '18:10:00'),
+(18, 6, 'Noon', '00:02:00'),
+(19, 6, 'First', '06:35:00'),
+(20, 6, 'Second', '09:18:00'),
+(21, 5, 'Matinee', '20:04:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_theatre`
+--
+
+CREATE TABLE `tbl_theatre` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `place` varchar(100) NOT NULL,
+  `state` varchar(50) NOT NULL,
+  `pin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_theatre`
+--
+
+INSERT INTO `tbl_theatre` (`id`, `name`, `address`, `place`, `state`, `pin`) VALUES
+(3, 'BJML', 'KATHMANDU ', 'PATAN', 'DM', 691523);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_bookings`
+--
+ALTER TABLE `tbl_bookings`
+  ADD PRIMARY KEY (`book_id`) USING BTREE;
+
+--
+-- Indexes for table `tbl_login`
+--
+ALTER TABLE `tbl_login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_movie`
+--
+ALTER TABLE `tbl_movie`
+  ADD PRIMARY KEY (`movie_id`);
+
+--
+-- Indexes for table `tbl_news`
+--
+ALTER TABLE `tbl_news`
+  ADD PRIMARY KEY (`news_id`);
+
+--
+-- Indexes for table `tbl_registration`
+--
+ALTER TABLE `tbl_registration`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `tbl_screens`
+--
+ALTER TABLE `tbl_screens`
+  ADD PRIMARY KEY (`screen_id`);
+
+--
+-- Indexes for table `tbl_shows`
+--
+ALTER TABLE `tbl_shows`
+  ADD PRIMARY KEY (`s_id`),
+  ADD UNIQUE KEY `s_id` (`s_id`,`st_id`);
+
+--
+-- Indexes for table `tbl_show_time`
+--
+ALTER TABLE `tbl_show_time`
+  ADD PRIMARY KEY (`st_id`);
+
+--
+-- Indexes for table `tbl_theatre`
+--
+ALTER TABLE `tbl_theatre`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_bookings`
+--
+ALTER TABLE `tbl_bookings`
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `tbl_login`
+--
+ALTER TABLE `tbl_login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `tbl_movie`
+--
+ALTER TABLE `tbl_movie`
+  MODIFY `movie_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `tbl_news`
+--
+ALTER TABLE `tbl_news`
+  MODIFY `news_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `tbl_registration`
+--
+ALTER TABLE `tbl_registration`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tbl_screens`
+--
+ALTER TABLE `tbl_screens`
+  MODIFY `screen_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tbl_shows`
+--
+ALTER TABLE `tbl_shows`
+  MODIFY `s_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `tbl_show_time`
+--
+ALTER TABLE `tbl_show_time`
+  MODIFY `st_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `tbl_theatre`
+--
+ALTER TABLE `tbl_theatre`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
